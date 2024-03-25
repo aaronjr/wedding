56b31fb4fe5604463b9c3bcdd0d39e573d21f731
Aaron Richards <aaron_richards@hotmail.co.uk>
2024-03-25 21:06:47 +0000

    more changes to visuals and responsiceness

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,10 @@
-const yes = document.querySelector('#yes')
-const no = document.querySelector('#no')
-const form = document.querySelector('form')
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+const form = document.querySelector("form");
 
-yes.addEventListener('click', ()=>{
-  form.className='visible'
-})
+yes.addEventListener("click", () => {
+  form.className = "visible";
+  if (window.innerHeight > window.innerWidth) {
+    window.scroll(0, 10000);
+  }
+});
