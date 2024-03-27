440965a54a0c15cf716061ea5b7bd1d0d2e708f7
Aaron Richards <aaron_richards@hotmail.co.uk>
2024-03-27 21:36:11 +0000

    updated many visuals and layouts accross pages

--- controllers/all.js
+++ controllers/all.js
@@ -1,5 +1,6 @@
 const Person = require("../models/Person");
 const No = require("../models/No");
+// const Group = require("../models/group");
 
 const asyncHandler = require("express-async-handler");
 
@@ -7,6 +8,8 @@ const asyncHandler = require("express-async-handler");
 exports.all_get = asyncHandler(async (req, res, next) => {
   const coming = await Person.find().sort({ group: 1, name: 1 }).exec();
   const notComing = await No.find().exec();
+  //const groups = await Group.find().exec();
+
   res.render("all.pug", {
     coming: coming,
     notComing: notComing,
