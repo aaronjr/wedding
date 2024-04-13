722595885e110e9fd76a63d645f87fb23bf79c8f
Aaron Richards <aaron_richards@hotmail.co.uk>
2024-04-13 14:11:25 +0100

    tidy up app.js

--- controllers/all.js
+++ controllers/all.js
@@ -1,24 +1,11 @@
 const Person = require("../models/Person");
 const No = require("../models/No");
-const Group = require("../models/group");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
 exports.all_get = asyncHandler(async (req, res, next) => {
   const coming = await Person.find().sort({ group: 1, name: 1 }).exec();
   const notComing = await No.find().exec();
-
-  // const grouped = coming.reduce((obj, item) => {
-  //   if (!obj[item.group]) {
-  //     obj[item.group] = [];
-  //   }
-  //   obj[item.group];
-  //   return obj;
-  // }, {});
-
-  // coming.map((person) => {
-  //   grouped[person.group].push(person)
-  // });
 
   const grouped = coming.reduce((obj, person) => {
     const { group } = person;
