741bcf2fb99245dc498f811b13ec269d18f3c0b4
Aaron Richards <aaron_richards@hotmail.co.uk>
2024-04-13 14:05:29 +0100

    added grouping for data on /all

--- controllers/all.js
+++ controllers/all.js
@@ -1,16 +1,36 @@
 const Person = require("../models/Person");
 const No = require("../models/No");
-// const Group = require("../models/group");
-
+const Group = require("../models/group");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
 exports.all_get = asyncHandler(async (req, res, next) => {
   const coming = await Person.find().sort({ group: 1, name: 1 }).exec();
   const notComing = await No.find().exec();
-  //const groups = await Group.find().exec();
+
+  // const grouped = coming.reduce((obj, item) => {
+  //   if (!obj[item.group]) {
+  //     obj[item.group] = [];
+  //   }
+  //   obj[item.group];
+  //   return obj;
+  // }, {});
+
+  // coming.map((person) => {
+  //   grouped[person.group].push(person)
+  // });
+
+  const grouped = coming.reduce((obj, person) => {
+    const { group } = person;
+    if (!obj[group]) {
+      obj[group] = [];
+    }
+    obj[group].push(person);
+    return obj;
+  }, {});
 
   res.render("all.pug", {
+    grouped: grouped,
     coming: coming,
     notComing: notComing,
   });
