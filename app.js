89f9c6c7694cda60e28ac9de73692deac7ecf622
Aaron Richards <aaron_richards@hotmail.co.uk>
2024-05-13 07:57:58 +0100

    update button colours

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-//const url = require("./password.js");
+const url = require("./password.js");
 
 var indexRouter = require("./routes/index");
 
@@ -25,8 +25,7 @@ app.use(limiter);
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const mongoDB = process.env.MONGODB_URI;
-//|| url;
+const mongoDB = process.env.MONGODB_URI || url;
 
 main().catch((err) => console.log(err));
 async function main() {
